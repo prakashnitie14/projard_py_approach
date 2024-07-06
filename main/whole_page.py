b723040c3653a87648f7853a3f6c20f8ecc53796
Prakash <prakashnitie14@gmail.com>
2024-07-06 17:31:20 -0400

    Update extract_metric function to handle negative values

--- main/whole_page.py
+++ main/whole_page.py
@@ -40,6 +40,7 @@ def analyze_text(text):
     
     return company_name, quality_of_statements
 
+
 def extract_columns_regex(data):
     # Define the adjusted regex patterns to handle various row formats
     pattern_numeric = re.compile(r'(.+?)\s*([\$€£¥₹]?)\s*(-?\$?\(?[\d,]+(?:\.\d{1,2})?\)?)\s*([\$€£¥₹]?)\s*(-?\$?\(?[\d,]+(?:\.\d{1,2})?\)?)')
@@ -78,6 +79,14 @@ def extract_columns_regex(data):
             print(f"Row does not match pattern or is empty: {row}")
 
     return structured_data
+
+# Clean up the structured data
+def clean_parameter(param):
+    # Remove special characters, numbers, and certain words
+    param = re.sub(r'[()\[\]{}#@!$%^&*]', '', param)  # Remove special characters
+    param = re.sub(r'\d+', '', param)  # Remove numbers
+    param = re.sub(r'\b(Note|note)\b', '', param)  # Remove the word "Note"
+    return param.strip()
 
 def extract_tables_from_pages(pdf_path, page_numbers):
     """
@@ -128,7 +137,6 @@ def save_tables_to_csv(structured_data, filename):
 def split_into_tables(df):
     income_statement_table = []
     balance_sheet_table = []
-    cashflow_table = []
     current_table = None  # To track which table (income statement or balance sheet)
 
     for index, row in df.iterrows():
@@ -136,21 +144,17 @@ def split_into_tables(df):
             current_table = 'Income Statement'
         elif is_balance_sheet_start(row['Parameter']):
             current_table = 'Balance Sheet'
-        elif is_cashflow_start(row['Parameter']):
-            current_table = 'Cashflow Statement'
 
         if current_table == 'Income Statement':
             income_statement_table.append(row)
         elif current_table == 'Balance Sheet':
             balance_sheet_table.append(row)
-        elif current_table == 'Cashflow Statement':
-            cashflow_table.append(row)
 
-    return income_statement_table, balance_sheet_table, cashflow_table
+    return income_statement_table, balance_sheet_table
 
 # Example functions to check if a row indicates the start of Income Statement or Balance Sheet
 def is_income_statement_start(parameter):
-    keywords = ['Total Sales']
+    keywords = ['Total Sales', 'Sales','Income','Operating revenues']
     for keyword in keywords:
         if keyword.lower() in parameter.lower():
             return True
@@ -163,40 +167,35 @@ def is_balance_sheet_start(parameter):
             return True
     return False
 
-def is_cashflow_start(parameter):
-    keywords = ['Statement of Cashflows']
-    for keyword in keywords:
-        if keyword.lower() in parameter.lower():
-            return True
-    return False
 
 # Example usage with your PDF file
-pdf_path = "/Users/prakashsethu/Documents/Whole Balance sheets - updated - v2 (v1 errors removed)/Company 3- Full Balance Sheet - Sheet1.pdf"
+pdf_path = "/Users/prakashsethu/Documents/Whole Balance sheets - updated - v2 (v1 errors removed)/2024_Q1_Financial_Statements_MDA_Final.pdf"
 
 # Specify the page numbers (1-indexed)
-page_numbers = [1,2,3,4,5,6,7,8]  # Example page numbers: for balance sheet,  income statement and cashflow statement
+page_numbers = [1,2]  # Example page numbers: for balance sheet and income statement
+
+#bbox = (0.0, 0.0, 612.0, 79)   # Example values; adjust based on your document
 
 # Extract tables from the PDF
 structured_data = extract_tables_from_pages(pdf_path, page_numbers)
 
-# Save extracted tables to CSV
-DataFrame_Format = save_tables_to_csv(structured_data, "extracted_table.csv")
+cleaned_data = [(clean_parameter(param), cur1, cur_val, cur2, prev_val) for param, cur1, cur_val, cur2, prev_val in structured_data]
 
-income_statement_table, balance_sheet_table, cashflow_table = split_into_tables(DataFrame_Format)
+
+# Save extracted tables to CSV
+DataFrame_Format = save_tables_to_csv(cleaned_data, "extracted_table.csv")
+
+income_statement_table, balance_sheet_table = split_into_tables(DataFrame_Format)
 
     # Step 2: Print or further process the tables
 
 
 income_statement_df = pd.DataFrame(income_statement_table)
 balance_sheet_df = pd.DataFrame(balance_sheet_table)
-cashflow_df = pd.DataFrame(cashflow_table)
 
 # Print Income Statement Table as DataFrame
-# print("Income Statement Table:")
-# print(income_statement_df)
-
-print ("Cashflow Table")
-print (cashflow_df)
+print("Income Statement Table:")
+print(income_statement_df)
 
 # Print Balance Sheet Table as DataFrame
 #print("\nBalance Sheet Table:")
@@ -237,17 +236,6 @@ def extract_metric(df, metric_names, sum_values=False):
                 temp_current = row['Current year']
                 temp_previous = row['Previous year']
 
-                # Convert to float, handling exceptions
-                try:
-                    temp_current = float(temp_current)
-                except (TypeError, ValueError):
-                    temp_current = None
-                
-                try:
-                    temp_previous = float(temp_previous)
-                except (TypeError, ValueError):
-                    temp_previous = None
-
                 if sum_values:
                     if temp_current is not None and temp_current != 0:
                         current_year_value += temp_current
@@ -279,8 +267,8 @@ def extract_metrics(income_statement_df, balance_sheet_df):
         'Gross Profit': ['Gross Profit', 'Gross Margins'],
         'NIBT': ['Net Income Before Taxes', 'Income before taxes', 'Net Profit Before Taxes','Earnings before income taxes'],
         'Interest Expense': ['Interest Expense', 'Interest Cost', 'Interest and Bank charges','Interest on long term debt'],
-        'Depreciation': ['Depreciation', 'Depreciation Expense', 'Amortization', 'Depreciation/Amortization'],
-        'One time gains (loss)': ['Gain from Sale of Assets','Gains through sale of property','Losses through sale of property','Gains (losses) through sale of property','Gains/ losses through sale of property','Gains/losses through sale of property','Loss through sale of property','Gains (loss) through sale of property','Gains/ loss through sale of property','Gains/loss through sale of property'],
+        'Depreciation': ['Depreciation', 'Depreciation Expense', 'Amortization', 'Depreciation/Amortization','Depreciation and amortization'],
+        'One time gains (loss)': ['Gain from Sale of Assets','Gains through sale of property','Losses through sale of property','Gains (losses) through sale of property','Gains/ losses through sale of property','Gains/losses through sale of property','Loss through sale of property','Gains (loss) through sale of property','Gains/ loss through sale of property','Gains/loss through sale of property','Income from equity investments'],
         'FX Gains/ (loss)': ['FX Gains', 'FX Losses', 'FX Gains/Losses','FX Loss', 'FX Gains/Loss','Foreign exchange gains (losses)',
                              'Loss (gain) on foreign exchange'],
         'Govt Grant': ['Government Grant', 'Government Grants', 'CEWS','Govt Grant']
@@ -392,116 +380,6 @@ def extract_metrics(income_statement_df, balance_sheet_df):
 
 extracted_data = extract_metrics(income_statement_df, balance_sheet_df)
 
-def extract_dsc_table(income_statement_df, cashflow_df, extracted_data):
-
-    # Extract Unfinanced Capex from Cashflow DataFrame
-    capex_names = ["Purchase of new Property Plant and Equipment","Proceeds from sale of PP&E"]
-    unfinanced_capex_current_temp, unfinanced_capex_previous_temp = extract_metric(cashflow_df, capex_names, sum_values=True)
-
-    unfinanced_capex_current = 0
-    unfinanced_capex_previous = 0
-
-    if unfinanced_capex_current_temp < 0:
-        unfinanced_capex_current = unfinanced_capex_current_temp
-    
-    if unfinanced_capex_current_temp < 0:
-        unfinanced_capex_previous = unfinanced_capex_previous_temp
-
-    # Extract Distributions from Income Statement DataFrame
-    distributions_names = ["Dividends Paid","Due to Shareholders","due from Related Parties","Due from Shareholders","Due from Directors"]
-    distributions_current, distributions_previous = extract_metric(cashflow_df, distributions_names, sum_values=True)
-
-    # Extract Cash Taxes from Income Statement DataFrame
-    cash_taxes_names = ["Income taxes"]
-    cash_taxes_current, cash_taxes_previous = extract_metric(income_statement_df, cash_taxes_names, sum_values=True)
-
-    # Extract more Cash Tax items from Cashflow Statement DataFrame
-    cash_taxes_names1 = ["Income tax payable","Income tax receivable"]
-    cash_taxes_current1, cash_taxes_previous1 = extract_metric(cashflow_df, cash_taxes_names1, sum_values=True)
-
-
-    # Retrieve Adjusted EBITDA from extracted data
-    adjusted_ebitda_current = extracted_data['Adjusted EBITDA']['Current Year']
-    adjusted_ebitda_previous = extracted_data['Adjusted EBITDA']['Previous Year']
-
-    # Retrieve Interest Cost from extracted data
-    interest_expense_current = extracted_data['Interest Expense']['Current Year']
-    interest_expense_previous = extracted_data['Interest Expense']['Previous Year']
-
-    # Extract Repayment from Cashflow Statement DataFrame
-    cash_taxes_names1 = ["Repayment of long term loan","Repayment of capital lease obligations"]
-    repayment_amount_current, repayment_amount_previous = extract_metric(cashflow_df, cash_taxes_names1, sum_values=True)
-
-    FCF_current = adjusted_ebitda_current + unfinanced_capex_current + distributions_current + (cash_taxes_current*-1)+cash_taxes_current1
-
-    FCF_previous = adjusted_ebitda_previous + unfinanced_capex_previous + distributions_previous + (cash_taxes_previous*-1)+cash_taxes_previous1
-
-    debt_service_cost_current = interest_expense_current + repayment_amount_current
-    debt_service_cost_previous = interest_expense_current + repayment_amount_current
-
-    # Create the DSC Table
-    dsc_table = [
-        {
-            'Parameter': 'Adjusted EBITDA',
-            'Current Year': adjusted_ebitda_current,
-            'Previous Year': adjusted_ebitda_previous
-        },
-        {
-            'Parameter': 'Unfinanced Capex',
-            'Current Year': unfinanced_capex_current,
-            'Previous Year': unfinanced_capex_previous
-        },
-        {
-            'Parameter': 'Distributions',
-            'Current Year': distributions_current,
-            'Previous Year': distributions_previous
-        },
-        {
-            'Parameter': 'Cash Taxes',
-            'Current Year': (cash_taxes_current*-1)+cash_taxes_current1,
-            'Previous Year': (cash_taxes_previous*-1)+cash_taxes_previous1
-        },
-        {
-            'Parameter': 'Free Cashflow (FCF)',
-            'Current Year': FCF_current,
-            'Previous Year': FCF_previous
-        },
-        {
-            'Parameter': 'Interest Expense',
-            'Current Year': interest_expense_current,
-            'Previous Year': interest_expense_previous
-        },
-        {
-            'Parameter': 'Principal Repayment',
-            'Current Year': repayment_amount_current,
-            'Previous Year': repayment_amount_previous
-        },
-        {
-            'Parameter': 'Debt Service Expense',
-            'Current Year': debt_service_cost_current,
-            'Previous Year': debt_service_cost_previous
-        },
-        {
-            'Parameter': 'Debt Service Coverage (DSC)',
-            'Current Year': FCF_current / (debt_service_cost_current),
-            'Previous Year': FCF_previous / (debt_service_cost_previous)
-        },
-        {
-            'Parameter': 'FCF Headroom',
-            'Current Year': (FCF_current - debt_service_cost_current*1.1),
-            'Previous Year': (FCF_previous - debt_service_cost_previous*1.1)
-        }
-    ]
-
-    return pd.DataFrame(dsc_table)
-
-# Example usage with extracted dataframes and metrics
-dsc_df = extract_dsc_table(income_statement_df, cashflow_df, extracted_data)
-
 # Convert the extracted data to a DataFrame for display
 extracted_df = pd.DataFrame.from_dict(extracted_data, orient='index')
-print ('This is operating financial statements')
 print(extracted_df)
-
-print ('Debt service coverage table')
-print(dsc_df)
