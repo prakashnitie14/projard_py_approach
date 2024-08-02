5ed0658130c79e52ed7ac2418189842401a9ec7f
prakashnitie14 <137127820+prakashnitie14@users.noreply.github.com>
2024-08-02 12:25:58 -0400

    Delete main/java/com/corporate/riskmanagement directory

--- main/java/com/corporate/riskmanagement/repository/DocumentMetadataCrudRepo.java
+++
@@ -1,9 +0,0 @@
-package com.corporate.riskmanagement.repository;
-
-import com.corporate.riskmanagement.entities.DocumentMetadata;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DocumentMetadataCrudRepo extends CrudRepository<DocumentMetadata, Long> {
-}
--- main/java/com/corporate/riskmanagement/repository/EmployeeCrudRepo.java
+++
@@ -1,8 +0,0 @@
-package com.corporate.riskmanagement.repository;
-
-import com.corporate.riskmanagement.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface EmployeeCrudRepo extends JpaRepository<Employee, Long> {
-}
